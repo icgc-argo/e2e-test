b8b97fb7414deb62827cbc2244a4b9bcca39b1ce
Jon Eubank <joneubank@gmail.com>
2019-08-12 12:31:01 -0400

    :sparkles: Helpers gets login flow method and test users

--- helpers.js
+++ helpers.js
@@ -12,6 +12,21 @@ const BS_USER = process.env.BROWSERSTACK_USER;
 const BS_KEY = process.env.BROWSERSTACK_ACCESS_KEY;
 const BS_API_ROOT = process.env.BROWSERSTACK_API_ROOT;
 
+const TEST_USERS = {
+  DCC_ADMIN: {
+    email: process.env.LOGIN_USERID_DCCADMIN,
+    pass: process.env.LOGIN_USERPASS_DCCADMIN,
+  },
+  PRGOGRAM_ADMIN_SINGLE: {
+    email: process.env.LOGIN_USERID_PROGRAM_ADMIN_SINGLE,
+    pass: process.env.LOGIN_USERPASS_PROGRAM_ADMIN_SINGLE,
+  },
+  PRGOGRAM_ADMIN_MULTI: {
+    email: process.env.LOGIN_USERID_PROGRAM_ADMIN_MULTI,
+    pass: process.env.LOGIN_USERPASS_PROGRAM_ADMIN_MULTI,
+  },
+};
+
 const updateStatus = (browser, status, reason) => {
   const sessionId = browser.sessionId;
 
@@ -48,10 +63,27 @@ const runGqlQuery = ({ query, variables }) =>
     }),
   }).then(res => res.json());
 
-const visitPath = browser => path => browser.url(urlJoin(process.env.UI_ROOT, path));
+const visitPath = browser => path => browser.url(buildUrl(path));
+
+const buildUrl = path => urlJoin(process.env.UI_ROOT, path);
+
+const loginAs = browser => user =>
+  visitPath(browser)('/')
+    .waitForElementVisible('#google-login')
+    .click('#google-login')
+    .waitForElementVisible('input[type="email"]')
+    .setValue('input[type="email"]', user.email)
+    .click('#identifierNext')
+    .waitForElementVisible('input[type="password"]')
+    .setValue('input[type="password"]', user.pass)
+    .click('#passwordNext')
+    .waitForElementVisible('nav', 20000);
 
 module.exports = {
   runGqlQuery,
   updateStatus,
   visitPath,
+  buildUrl,
+  loginAs,
+  TEST_USERS,
 };
