0ecb2c173a87e05455558b2bac1dc137ea2515e5
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2020-07-20 00:26:04 -0400

    add test generator (#39)
    * add test generator
    
    * add test name for browserstack
    
    * add developer to test template
    
    * add developer name to BS test name

---
+++ plop/plopfile.ts
@@ -0,0 +1,23 @@
+import { NodePlopAPI } from 'plop';
+
+module.exports = function(plop: NodePlopAPI) {
+  plop.setGenerator('test', {
+    description: 'Create a test file',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter test name...',
+      },
+      { type: 'input', name: 'group', message: 'Please enter test group name...' },
+      { type: 'input', name: 'developer', message: 'Please enter your name (test developer)...' },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: './src/tests/{{dashCase group}}/{{dashCase name}}.ts',
+        templateFile: './plop/templates/test.hbs',
+      },
+    ],
+  });
+};
