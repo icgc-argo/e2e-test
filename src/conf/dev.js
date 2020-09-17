543aa2c03daac3583c8204c9270d08d45c25ea49
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-09-17 18:36:53 -0400

    change default runner to FireFox

--- src/conf/dev.js
+++ src/conf/dev.js
@@ -2,28 +2,25 @@ const { CHROME_LATEST } = require('../env/dev');
 const path = require('path');
 require('dotenv').config();
 
-const CHROME_DRIVER_PATH = process.env.CHROME_DRIVER_PATH;
-
 const config = {
   src_folders: ['../tests'],
   output_folder: './test_reports',
 
-  //  custom_commands_path: './commands',
+  // custom_commands_path: './commands',
 
   webdriver: {
     start_process: true,
-    server_path: 'node_modules/chromedriver/bin/chromedriver',
+    server_path: 'node_modules/geckodriver/bin/geckodriver',
     port: 9515,
   },
 
   test_settings: {
     default: {
       desiredCapabilities: {
-        browserName: 'chrome',
+        browserName: 'firefox',
         loggingPrefs: { browser: 'ALL' },
       },
     },
-    CHROME_LATEST,
   },
 };
 
